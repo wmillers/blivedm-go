a07e565874329efbcd34ff0908e7493130ddd4db
wmillers <31211860+wmillers@users.noreply.github.com>
2022-03-23 21:31:29 +0800

    add infos in Danmaku struct

--- message/danmu.go
+++ message/danmu.go
@@ -2,8 +2,7 @@ package message
 
 import (
 	"bytes"
-	"encoding/json"
-	log "github.com/sirupsen/logrus"
+
 	"github.com/tidwall/gjson"
 )
 
@@ -14,39 +13,43 @@ const (
 
 type (
 	Danmaku struct {
-		Sender    *User
-		Content   string
-		Extra     *Extra
-		Emoticon  *Emoticon
-		Type      int
+		Mode      int64
+		FontSize  int64
+		Color     int64
 		Timestamp int64
+		Rnd       int64
+		UID_CRC32 string
+		MsgType   int64
+		Bubble    int64
+
+		Msg string
+
+		Uid          int64
+		Uname        string
+		Admin        bool
+		Vip          bool
+		Svip         bool
+		Urank        int64
+		MobileVerify bool
+		UnameColor   string
+
+		MedalLevel   int64
+		MedalName    string
+		MedalUpName  string
+		MedalRoomId  int64
+		MedalColor   int64
+		SpecialMedal string
+
+		UserLevel      int64
+		UserLevelColor int64
+		UserLevelRank  string
+
+		OldTitle string
+		Title    string
+
+		PrivilegeType int64
 	}
 
-	Extra struct {
-		SendFromMe     bool   `json:"send_from_me"`
-		Mode           int    `json:"mode"`
-		Color          int    `json:"color"`
-		DmType         int    `json:"dm_type"`
-		FontSize       int    `json:"font_size"`
-		PlayerMode     int    `json:"player_mode"`
-		ShowPlayerType int    `json:"show_player_type"`
-		Content        string `json:"content"`
-		UserHash       string `json:"user_hash"`
-		EmoticonUnique string `json:"emoticon_unique"`
-		Direction      int    `json:"direction"`
-		PkDirection    int    `json:"pk_direction"`
-		SpaceType      string `json:"space_type"`
-		SpaceUrl       string `json:"space_url"`
-	}
-	Emoticon struct {
-		BulgeDisplay   int    `json:"bulge_display"`
-		EmoticonUnique string `json:"emoticon_unique"`
-		Height         int    `json:"height"`
-		InPlayerArea   int    `json:"in_player_area"`
-		IsDynamic      int    `json:"is_dynamic"`
-		Url            string `json:"url"`
-		Width          int    `json:"width"`
-	}
 	CommonNoticeDanmaku struct {
 		ContentSegments []struct {
 			FontColor string `json:"font_color"`
@@ -60,29 +63,38 @@ type (
 
 func (d *Danmaku) Parse(data []byte) {
 	sb := bytes.NewBuffer(data).String()
-	info := gjson.Get(sb, "info")
-	ext := new(Extra)
-	emo := new(Emoticon)
-	err := json.Unmarshal([]byte(info.Get("0.15.extra").String()), ext)
-	if err != nil {
-		log.Error("parse danmuku extra failed")
+	Get := func(place string) gjson.Result {
+		return gjson.Get(sb, place)
 	}
-	err = json.Unmarshal([]byte(info.Get("0.13").String()), emo)
-	if err != nil {
-		log.Error("parse danmuku emoticon failed")
+	d = &Danmaku{
+		Mode:           Get("info.0.1").Int(),
+		FontSize:       Get("info.0.2").Int(),
+		Color:          Get("info.0.3").Int(),
+		Timestamp:      Get("info.0.4").Int(),
+		Rnd:            Get("info.0.5").Int(),
+		UID_CRC32:      Get("info.0.7").String(),
+		MsgType:        Get("info.0.9").Int(),
+		Bubble:         Get("info.0.10").Int(),
+		Msg:            Get("info.1").String(),
+		Uid:            Get("info.2.0").Int(),
+		Uname:          Get("info.2.1").String(),
+		Admin:          Get("info.2.2").Bool(),
+		Vip:            Get("info.2.3").Bool(),
+		Svip:           Get("info.2.4").Bool(),
+		Urank:          Get("info.2.5").Int(),
+		MobileVerify:   Get("info.2.6").Bool(),
+		UnameColor:     Get("info.2.7").String(),
+		MedalLevel:     Get("info.3.0").Int(),
+		MedalName:      Get("info.3.1").String(),
+		MedalUpName:    Get("info.3.2").String(),
+		MedalRoomId:    Get("info.3.3").Int(),
+		MedalColor:     Get("info.3.4").Int(),
+		SpecialMedal:   Get("info.3.5").String(),
+		UserLevel:      Get("info.4.0").Int(),
+		UserLevelColor: Get("info.4.2").Int(),
+		UserLevelRank:  Get("info.4.3").String(),
+		OldTitle:       Get("info.5.0").String(),
+		Title:          Get("info.5.1").String(),
+		PrivilegeType:  Get("info.5").Int(),
 	}
-	d.Content = info.Get("1").String()
-	d.Sender = &User{
-		Uid:   int(info.Get("2.0").Int()),
-		Uname: info.Get("2.1").String(),
-		Medal: &Medal{
-			Name:  info.Get("3.1").String(),
-			Level: int(info.Get("3.0").Int()),
-			Up:    info.Get("3.2").String(),
-		},
-	}
-	d.Extra = ext
-	d.Emoticon = emo
-	d.Type = int(info.Get("0.12").Int())
-	d.Timestamp = info.Get("0.4").Int()
 }
